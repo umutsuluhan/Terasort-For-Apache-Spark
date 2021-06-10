60bd49c4a5e5b496b52c4481163da148a4404d6a
Hasan Umut Suluhan <suluhanumut@gmail.com>
2021-06-10 19:41:30 +0300

    Adding comments to the source codes

--- teragen.py
+++ teragen.py
@@ -2,7 +2,9 @@ from pyspark import SparkContext, SparkConf
 
 import sys, os, string, random
 
-if __name__ == "__main__":
+
+if __name__ == "__main__":                                  
+	#Getting arguments and checking if conditions are met
 	if len(sys.argv) != 3:
 		print("Usage:")
 		print("teragen.py <number of rows> <output directory>")
@@ -18,11 +20,14 @@ if __name__ == "__main__":
 		print("This path already exists. Please remove it or enter new file path")
 		exit()
 
+	#Creating Spark Context 
 	conf = SparkConf().setAppName("Teragen")
 	sc = SparkContext(conf=conf)
 
+	#Generating rowID's
 	rowId = list(range(0, int(sys.argv[1])))
 
+	#Teragen operation 
 	rdd = sc.parallelize(rowId)
 	rdd2 = rdd.map(lambda x :( "".join((random.choice(string.ascii_uppercase) for i in range(10))), x, "".join((random.choice(string.ascii_uppercase) for i in range(78)))))
 	rdd2.saveAsTextFile(complete_path)
